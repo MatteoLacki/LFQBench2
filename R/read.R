6ee9712d677cee073309d013602a19580fbb0046
matteo <matteo.lacki@gmail.com>
2019-02-07 14:37:14 +0100

    Initial scripts for reading Isospec protein report into R and combining more of those.

---
+++ R/read.R
@@ -0,0 +1,36 @@
+require(dplyr)
+require(tidyr)
+
+#' Parse an excel ISOQuant protein report.
+#'
+#' Prepare the data in the long format, so that it is easier to combine it.
+#'
+#' @param path Path
+#' @param col_pattern A pattern that will select columns with intensities.
+#' @param sheet Which excell sheet should be imported?
+#' @param sample_no2bio_rep An additional data.frame mapping sample numbers to biological replicates.
+#' @return Standard long format data frame object.
+#' @export
+isoquant_report = function(path,
+                           col_pattern,
+                           sheet="TOP3 quantification",
+                           sample_no2bio_rep=NA)
+{
+  contains = dplyr::contains
+  DW = readxl::read_excel(path, sheet=sheet, skip=1)
+  DW$file = path
+  DW = dplyr::select(DW, -contains('AVERAGE'))
+  # averages are not interesting for the analysis and we can easily get them later on.
+  DL = tidyr::gather(DW, "condition", "intensity", contains(col_pattern))
+  colnames(DL) = make.names(colnames(DL))
+  DL = tidyr::separate(DL, condition, c('project', 'partner', 'run'), sep=" ")
+  get_k_part = function(strings, k) sapply(strsplit(strings, '-'), '[[', k)
+  DL = dplyr::mutate(DL, run = as.integer(run),
+              sample_no = as.integer(get_k_part(project, 3)))
+  if(typeof(sample_no2bio_rep) != 'logical'){
+    DL = dplyr::left_join(DL, sample_no2bio_rep, by='sample_no')
+  }
+  DL
+}
+
+
