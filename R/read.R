b7617dfa6a3e365550ce012610a20a60080a0def
matteo <matteo.lacki@gmail.com>
2019-03-06 13:51:01 +0100

    Getting rid of dplyr. Rewriting CLI.

--- R/read.R
+++ R/read.R
@@ -1,74 +1,64 @@
-require(dplyr)
-require(tidyr)
-
-#' Parse a full excel ISOQuant protein report.
-#'
-#' Prepare the data in the long format, so that it is easier to combine it.
+#' Read an ISOQuant protein report.
 #'
 #' @param path Path to the protein report.
-#' @param col_pattern A pattern that will select columns with intensities.
-#' @param sheet Which excell sheet should be imported?
-#' @param sample_no2bio_rep An additional data.frame mapping sample numbers to biological replicates.
-#' @return Standard long format data frame object.
+#' @param long_df Should the output be in the long format? [default = FALSE]
+#' @param intensity_pattern A pattern that will select columns with intensities [default = '[A|B][:space:][:digit:]']
+#' @param sheet Which excell sheet should be imported [default="TOP3 quantification"]
+#' @return Wide or long data.table
 #' @export
-protein_report = function(path,
-                          col_pattern,
-                          sheet="TOP3 quantification",
-                          sample_no2bio_rep=NA)
+read_isoquant_protein_report = function(path, long_df=FALSE, intensity_pattern="[A|B][:space:][:digit:]", sheet="TOP3 quantification")
 {
-  contains = dplyr::contains
-  DW = readxl::read_excel(path, sheet=sheet, skip=1)
-  DW$file = path
-  DW = dplyr::select(DW, -contains('AVERAGE'))
-  # averages are not interesting for the analysis and we can easily get them later on.
-  DL = tidyr::gather(DW, "condition", "intensity",
-                     contains(col_pattern), na.rm = T)
-  # na.rm makes the long format meaningfully smaller.
-  colnames(DL) = make.names(colnames(DL))
-
-  # THIS MIGHT BE TOO SPECIFIC....
-  DL = tidyr::separate(DL, condition, c('project', 'partner', 'run'), sep=" ")
-  get_k_part = function(strings, k) sapply(strsplit(strings, '-'), '[[', k)
-  DL = dplyr::mutate(DL, run = as.integer(run),
-              sample_no = as.integer(get_k_part(project, 3)))
-  if(typeof(sample_no2bio_rep) != 'logical'){
-    DL = dplyr::left_join(DL, sample_no2bio_rep, by='sample_no')
-  }
-  DL
+  o = as.data.table(read_excel(path, sheet=sheet, skip=1))
+  o[o == ""] = NA
+  o[, path:=path][, grep("AVERAGE", colnames(o)):=NULL] # add path, remove AVEAGEs
+  if(long_df) o = melt(o,
+                       measure.vars=str_which(colnames(o), intensity_pattern),
+                       na.rm=T,
+                       variable.factor=F,
+                       variable.name="cond",
+                       value.name="intensity")
+  return(o)
 }
 
 
-
-#' Parse a ISOQuant csv peptide quantification report.
-#'
-#' Prepare the data in the long format, so that it is easier to combine it.
+#' Read an ISOQuant csv peptide report.
 #'
 #' @param path Path to the report.
-#' @param col_pattern A pattern that will select columns with intensities.
-#' @return Standard long format data frame object.
+#' @param long_df Should the output be in the long format? [default = FALSE]
+#' @param intensity_pattern A pattern that will select columns with intensities [default = "intensity in"]
+#' @return Wide or long data.table
 #' @export
-peptide_quantification_report = function(path,
-                                         col_pattern)
+read_isoquant_peptide_report = function(path, long_df = FALSE, intensity_pattern = "intensity in")
 {
-  DW = readr::read_csv(path)
-  DW$file = path
-  DL = tidyr::gather(DW, "condition", "intensity",
-                     dplyr::contains(col_pattern), na.rm = T)
-  DL
+  o = fread(path)
+  o[o == ""] = NA
+  o$path = path
+  if(long_df) o = melt(o,
+                       measure.vars=patterns(intensity_pattern),
+                       na.rm=T,
+                       variable.factor=F,
+                       variable.name="cond",
+                       value.name="intensity")
+  return(o)
 }
 
-#' Parse a simple ISOQuant csv protein quantification report.
-#'
-#' Prepare the data in the long format, so that it is easier to combine it.
+
+#' Read a simple protein ISOQuant report.
 #'
 #' @param path Path to the report.
-#' @return Standard long format data frame object.
+#' @param long_df Should the output be in the long format? [default = FALSE]
+#' @param intensity_pattern A pattern that will select columns with intensities [default = '[A|B][:space:][:digit:]']
+#' @return Wide or long data.table
 #' @export
-simple_protein_report = function(path)
-{
-  DW = readr::read_csv(path)
-  DL = tidyr::gather(DW, "condition", "intensity", -entry, na.rm = T)
-  DL$file = path
-  DL
+read_isoquant_simple_protein_report = function(path, long_df=FALSE, intensity_pattern="[A|B][:space:][:digit:]"){
+  o = fread(path)
+  o[o == ""] = NA
+  o$path = path
+  if(long_df) o = melt(o, measure.vars=str_which(colnames(o), intensity_pattern),
+                          na.rm=T,
+                          variable.factor=F,
+                          variable.name="cond",
+                          value.name="intensity")
+  return(o)
 }
 
