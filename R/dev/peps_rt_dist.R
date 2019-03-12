fc56a248a12c2bb793adb48156dae0493715070c
matteo <matteo.lacki@gmail.com>
2019-03-12 16:59:11 +0100

    minor

--- R/dev/peps_rt_dist.R
+++ R/dev/peps_rt_dist.R
@@ -13,11 +13,13 @@ ids = D$id # which peptide was measured
 
 S = get_smoothed_data(rt, runs, ids) # get the data for plotting
 S[,run:=ordered(run)] # change run to ordered factor, for ggplot to be happy
-z = plot_dist_to_reference(S)
+plot_dist_to_reference(S)
 
-ggsave(filename=path.expand("~/Projects/LFQBench2/LFQBench2/picts/dist2meds.jpg"), z, width=25, height=10)
+z = plot_dist_to_reference(S)
+cowplot::ggsave(filename=path.expand("~/Projects/LFQBench2/LFQBench2/picts/dist2meds.jpg"), z, width=25, height=10)
 
 
 Z <- S
-Z[, `:=`(top=NULL, bot=NULL)]
-plot_dist_to_reference(Z)
+S[, `:=`(top=NULL, bot=NULL)]
+z2 = plot_dist_to_reference(Z)
+cowplot::ggsave(filename=path.expand("~/Projects/LFQBench2/LFQBench2/picts/dist2meds2.jpg"), z2, width=25, height=10)
